d36e6eedacdc3c163b77d4bd3f7181833f1e30f9
Russell Haering <russellhaering@gmail.com>
2010-03-01 23:00:53 -0500

    Added more search tests

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ class TestBookSearch(unittest.TestCase):
             })
             handle = urlopen(self.url, data)
             text = handle.read()
-            self.assertTrue(re.search("1234567890123", text)) 
-        
+            self.assertTrue(re.search("1234567890123", text))
+
         def testTitleHasNoResults(self):
             data = urlencode({
                 'title': 'Mastermind',
@@ -31,14 +31,6 @@ class TestBookSearch(unittest.TestCase):
             text = handle.read()
             self.assertTrue(re.search("Criminal", text))
 
-        def testPartialISBNHasResults(self):
-            data = urlencode({
-                'isbn': '12345',
-            })
-            handle = urlopen(self.url, data)
-            text = handle.read()
-            self.assertTrue(re.search("Criminal", text))
-
         def testFullISBNHasNoResults(self):
             data = urlencode({
                 'title': '3210987654321',
@@ -46,3 +38,73 @@ class TestBookSearch(unittest.TestCase):
             handle = urlopen(self.url, data)
             text = handle.read()
             self.assertTrue(re.search("No results were found", text))
+
+        def testAuthorPartialHasResults(self):
+            data = urlencode({
+                'authorName': 'James',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("1234567890123", text))
+
+        def testAuthorPartialHasNoReults(self):
+            data = urlencode({
+                'authorName': 'Orwell',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("No results were found", text))
+
+        def testKeywordHasResults(self):
+            data = urlencode({
+                'keyword': 'First',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("0000000000001", text))
+
+        def testKeywordHasNoResults(self):
+            data = urlencode({
+                'keyword': 'Last',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("No results were found", text))
+
+        def testAuthorKeywordHasResults(self):
+            data = urlencode({
+                'keyword': 'exploits',
+                'authorName': 'James',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("1234567890123", text))
+
+        def testAuthorTitleHasResults(self):
+            data = urlencode({
+                'title': 'Criminal',
+                'authorName': 'James',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("1234567890123", text))
+
+        def testAuthorISBNHasResults(self):
+            data = urlencode({
+                'isbn': '1234567890123',
+                'authorName': 'James',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("1234567890123", text))
+
+        def testAllHasResults(self):
+            data = urlencode({
+                'title': 'Criminal',
+                'isbn': '1234567890123',
+                'authorName': 'James',
+                'keyword': 'exploits',
+            })
+            handle = urlopen(self.url, data)
+            text = handle.read()
+            self.assertTrue(re.search("1234567890123", text))
